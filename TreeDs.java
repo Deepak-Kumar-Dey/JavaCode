2d4e987417e26515d0c283644fa05adc7d79cb7a
Deepak-Kumar-Dey <deepakkumardey850@gmail.com>
2025-08-21 19:36:34 +0530

    first commit

---
+++ TreeDs.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int v) {
+        data = v;
+        left = right = null;
+    }
+}
+
+
+class BinaryTree {
+    Node root;
+
+   
+    public static void printPreorder(Node node) {
+        if (node == null)
+            return;
+
+        System.out.print(node.data + " ");
+        printPreorder(node.left);
+        printPreorder(node.right);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.right = new Node(6);
+
+        BinaryTree.printPreorder(tree.root);
+    }
+}
